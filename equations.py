d668b356ca69e79d73d94cc7e0f3de7fe1988d6f
Rahil69 <raahil2005@hotmail.com>
2026-06-24 09:39:39 +0500

    correcting order

--- equations.py
+++ equations.py
@@ -1,34 +1,32 @@
 import math
 
-def great_circle_distance(latitudeA, longitudeA,latitudeB, longitudeB):
+def great_circle_distance(latitudeA, longitudeA, latitudeB, longitudeB):
     phi1 = math.radians(latitudeA)
     lambda1 = math.radians(longitudeA)
 
     phi2 = math.radians(latitudeB)
     lambda2 = math.radians(longitudeB)
 
-    #delta Y
-    delta_lambda = math.fabs(lambda2 - lambda1)
-    central_angle = \
-    math.atan2 \
-    (
-        math.sqrt(
-            math.pow(
-                math.cos(phi2) * math.sin(delta_lambda)
-                , 2.0
-            )
-            +
-            math.pow(
-                math.cos(phi1) * math.sin(phi2) - math.sin(phi1) * math.cos(phi2) *
-math.cos(delta_lambda)
-                , 2.0
-            )
-        )
+    delta_lambda = abs(lambda2 - lambda1)
+
+    y = math.sqrt(
+        (math.cos(phi2) * math.sin(delta_lambda)) ** 2
+        +
         (
-            math.sin(phi1) * math.sin(phi2) +
-            math.cos(phi1) * math.cos(phi2) *
-math.cos(delta_lambda)
-        )
+            math.cos(phi1) * math.sin(phi2)
+            - math.sin(phi1) * math.cos(phi2) * math.cos(delta_lambda)
+        ) ** 2
     )
+
+    x = (
+        math.sin(phi1) * math.sin(phi2)
+        +
+        math.cos(phi1) * math.cos(phi2) * math.cos(delta_lambda)
+    )
+
+    central_angle = math.atan2(y, x)
+
     R = 6371.009
     return R * central_angle
+
+print(great_circle_distance(20, 23, 52, 34))
